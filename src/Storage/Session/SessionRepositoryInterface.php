1f43550eabaf66bb3c5ac691d8e6bc619f5dec10
Parker Smith <parker@parkersmith.us>
2017-12-15 10:31:06 -0700

    Add storage repo interface

---
+++ src/Storage/Session/SessionRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parker
+ * Date: 12/15/17
+ * Time: 10:30 AM
+ */
+
+namespace App\Storage\Session;
+
+
+interface SessionRepositoryInterface
+{
+    public function has($key);
+    public function get($key);
+    public function put($key, $value);
+}
