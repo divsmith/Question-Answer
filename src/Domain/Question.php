6806f9fae4baf386118c2109b8a3ec72831e6b5c
Parker Smith <parker@parkersmith.us>
2017-12-14 17:29:12 -0700

    Add and test question model

--- src/Domain/Question.php
+++ src/Domain/Question.php
@@ -11,5 +11,48 @@ namespace App\Domain;
 
 class Question
 {
-    
+    private $uuid;
+    private $title;
+    private $text;
+    private $userEmail;
+
+    public function __construct($uuid, $title, $text, $userEmail)
+    {
+        if (empty($uuid) || empty($title) || empty($text) || empty($userEmail)) {
+            throw new \InvalidArgumentException("empty arguments");
+        }
+
+        if (!is_string($uuid) || !is_string($title) || !is_string($text) || !is_string($userEmail)) {
+            throw new \InvalidArgumentException("arguments are not strings");
+        }
+
+        if (!filter_var($userEmail, FILTER_VALIDATE_EMAIL)) {
+            throw new \InvalidArgumentException("email is not valid");
+        }
+
+        $this->uuid = $uuid;
+        $this->title = $title;
+        $this->text = $text;
+        $this->userEmail = $userEmail;
+    }
+
+    public function uuid()
+    {
+        return $this->uuid;
+    }
+
+    public function title()
+    {
+        return $this->title;
+    }
+
+    public function text()
+    {
+        return $this->text;
+    }
+
+    public function userEmail()
+    {
+        return $this->userEmail;
+    }
 }
---
+++ tests/QuestionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+class QuestionTest extends \PHPUnit_Framework_TestCase
+{
+    private $question;
+    private $uuid = "asdf1234";
+    private $title = "Test Title";
+    private $text = "This is my question...";
+    private $userEmail = "anne@example.com";
+
+    public function setUp()
+    {
+        $this->question = new \App\Domain\Question($this->uuid, $this->title, $this->text, $this->userEmail);
+    }
+    public function testQuestionEmptyArguments()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $question = new \App\Domain\Question("", "", "", "");
+    }
+
+    public function testQuestionArgumentsNotString()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $question = new \App\Domain\Question(1, 1, 1, 1);
+    }
+
+    public function testInvalidEmail()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $question = new \App\Domain\Question("1234", "1234", "1234", "1234");
+    }
+
+    public function testuuid()
+    {
+        $this->assertEquals($this->uuid, $this->question->uuid());
+    }
+
+    public function testGetTitle()
+    {
+        $this->assertEquals($this->title, $this->question->title());
+    }
+
+    public function testText()
+    {
+        $this->assertEquals($this->text, $this->question->text());
+    }
+
+    public function testUserEmail()
+    {
+        $this->assertEquals($this->userEmail, $this->question->userEmail());
+    }
+}
