cdacc6c054ff93098a8298e1e210ad800de05b57
Parker Smith <parker@parkersmith.us>
2017-12-15 10:23:44 -0700

    Add QuestionRepositoryInterface

---
+++ src/Storage/Question/QuestionRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parker
+ * Date: 12/15/17
+ * Time: 10:21 AM
+ */
+
+namespace App\Storage\Question;
+
+
+use App\Domain\Question;
+
+interface QuestionRepositoryInterface
+{
+    public function getByID($uuid);
+    public function getAllByUser($email);
+    public function getAll();
+    public function store(Question $question);
+}
