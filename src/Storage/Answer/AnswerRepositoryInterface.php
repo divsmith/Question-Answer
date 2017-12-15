ca7bef52ad3ae708899860a343ad41be51168f97
Parker Smith <parker@parkersmith.us>
2017-12-15 10:25:52 -0700

    Add AnswerRepositoryInterface

---
+++ src/Storage/Answer/AnswerRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parker
+ * Date: 12/15/17
+ * Time: 10:24 AM
+ */
+
+namespace App\Storage\Answer;
+
+
+use App\Domain\Answer;
+
+interface AnswerRepositoryInterface
+{
+    public function getByID($uuid);
+    public function getByQuestionID($uuid);
+    public function store(Answer $answer);
+}
