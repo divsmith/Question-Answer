e6add11a6f9dde56ae670e5a8be1dc6d9e681e4d
Parker Smith <parker@parkersmith.us>
2017-12-15 10:34:30 -0700

    Add user jack

---
+++ src/Storage/User/UserRepository.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parker
+ * Date: 12/15/17
+ * Time: 10:31 AM
+ */
+
+namespace App\Storage\User;
+
+
+use App\Domain\User;
+
+class UserRepository
+{
+    private $plugin;
+
+    public function __construct(UserRepositoryPluginInterface $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    public function getByEmail($email)
+    {
+        return $this->plugin->getByEmail($email);
+    }
+
+    public function store(User $user)
+    {
+        return $this->plugin->store($user);
+    }
+
+    public function getAll()
+    {
+        return $this->plugin->getAll();
+    }
+}
