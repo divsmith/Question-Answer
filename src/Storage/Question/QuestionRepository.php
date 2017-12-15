86a2f5303dd8bda878fdc51e10381fdadc101bce
Parker Smith <parker@parkersmith.us>
2017-12-15 10:37:06 -0700

    Add question jack

---
+++ src/Storage/Question/QuestionRepository.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parker
+ * Date: 12/15/17
+ * Time: 10:34 AM
+ */
+
+namespace App\Storage\Question;
+
+
+class QuestionRepository
+{
+    private $plugin;
+
+    public function __construct(QuestionRepositoryInterface $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+
+    public function getByID($uuid)
+    {
+        return $this->plugin->getByID($uuid);
+    }
+
+    public function getAllByUser($email)
+    {
+        return $this->plugin->getAllByUser($email);
+    }
+
+    public function getAll()
+    {
+        return $this->plugin->getAll();
+    }
+
+    public function store(Question $question)
+    {
+        return $this->plugin->store($question);
+    }
+}
