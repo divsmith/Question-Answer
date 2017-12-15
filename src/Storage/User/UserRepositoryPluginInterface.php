ab459559f705d75f43f7dee77b4f89e039c00291
Parker Smith <parker@parkersmith.us>
2017-12-15 10:21:07 -0700

    Add UserRepositoryInterface

---
+++ src/Storage/User/UserRepositoryPluginInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: parker
+ * Date: 12/15/17
+ * Time: 10:18 AM
+ */
+
+namespace App\Storage\User;
+
+
+use App\Domain\User;
+
+interface UserRepositoryPluginInterface
+{
+    public function getByEmail($email);
+    public function store(User $user);
+    public function getAll();
+}
